3c04631ca750d7681bb811b93e7efbfcb2e35ebd
kshitij879 <kshitij.gupta_cs18@gla.ac.in>
2019-08-22 21:35:22 +0530

    calculate speed and distance

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -7,6 +7,8 @@ package main;
 // import java.util.Scanner;
 // uncomment the above line to use input in your program.
 
+import java.util.Scanner;
+
 public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
@@ -15,6 +17,16 @@ public class Gravity {
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
 		 *   c. Calculate the result and print it to the screen.
 		 * */
+		double time;
+		double speed;
+		double distance;
+		double gravity = 9.8;
+		Scanner scanner = new Scanner(System.in);
+		time = scanner.nextDouble();
+		speed = gravity*time;
+		distance = 0.5*(gravity*time*time);
+		System.out.println("The speed of the object at " + time + " seconds after its release is " + speed + " and the distance the object has travelled in the " + time + " seconds after the relase is " + distance);
+		scanner.close();
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
